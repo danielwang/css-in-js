a4cbe78b9b6c0646eabbfbb1a9780066abec3b8e
Daniel Wang <daniel@campaignagent.com.au>
2021-08-04 11:55:01 +1000

    feat: add bpay component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import Bpay from './components/Bpay';
+import styled from 'styled-components';
 import './App.css';
+
+const Button = styled.button`
+    padding: 1rem;
+    background: red;
+`
+
+const Main = styled.main`
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    flex: 1 1 auto;
+    background-color: teal;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main>
+        <Button>click</Button>
+      </Main>
+      <Bpay billercode="12345" refnumber="98765" />
     </div>
   );
 }
---
+++ src/components/Bpay.js
@@ -0,0 +1,31 @@
+import bpaylogo from '../assets/images/bpay.png'; 
+import styles from './Bpay.module.scss';
+ 
+const Bpay = ({billercode,refnumber}) => {
+   return (
+     <>
+      <div className={styles.bpay}>
+        <div className={styles.panel}>
+          <div className={styles.icon}>
+              <img src={bpaylogo} width="50" />        
+          </div>
+          <div className={styles.ref}>
+            <div className={styles.data}>
+              <span>Biller Code:</span><span>{billercode}</span>          
+            </div>
+            <div className={styles.data}>
+              <span>Reference:</span><span>{refnumber}</span>
+            </div>
+          </div>
+        </div>
+       
+        <div className={styles.text}>
+          <b>Telephone & Intetrnet banking - PPAY®</b><br /> 
+          Contact your bank or financial institution to make this payment from your cheque, sayings or transaction account. More Info www.bpay.com.au
+        </div>
+       </div>
+     </>
+   )
+ }
+
+ export default Bpay;
